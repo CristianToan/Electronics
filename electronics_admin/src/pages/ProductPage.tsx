6464c96e20f8e0629614443860c9e7ff9ca86078
Tan Hung <maitanhung2@gmail.com>
2024-10-20 20:36:20 +0700

    fix product admin api

--- electronics_admin/src/pages/ProductPage.tsx
+++ electronics_admin/src/pages/ProductPage.tsx
@@ -424,10 +424,7 @@ const ProductPage = () => {
                 </table>
               </div>
               <div className="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800">
-                <span className="flex items-center col-span-3">
-                  {getAllProduct.data?.products_list.length}
-                </span>
-                <span className="flex col-span-6 mt-2 sm:mt-auto sm:justify-end">
+                <span className="flex col-span-9 mt-2 sm:mt-auto sm:justify-end">
                   <nav aria-label="Table navigation">
                     {getAllProduct?.data?.pagination.totalRecords >
                       getAllProduct?.data?.pagination.limit && (
