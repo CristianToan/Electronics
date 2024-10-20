cb3cbd77c2bed08e3e78c35e9618ba64abe04783
binva12 <quocdat3b@yahoo.com.vn>
2024-10-20 19:24:56 +0700

    change buildSlug in backend api

--- nodejs_backend_api/src/helpers/buildSlug.ts
+++ nodejs_backend_api/src/helpers/buildSlug.ts
@@ -1,84 +1,35 @@
 import slugify from "slugify";
 
-const vietnameseCharMap: { [key: string]: string } = {
-  à: "a",
-  á: "a",
-  ạ: "a",
-  ả: "a",
-  ã: "a",
-  â: "a",
-  ầ: "a",
-  ấ: "a",
-  ậ: "a",
-  ẩ: "a",
-  ẫ: "a",
-  ă: "a",
-  ằ: "a",
-  ắ: "a",
-  ặ: "a",
-  ẳ: "a",
-  ẵ: "a",
-  è: "e",
-  é: "e",
-  ẹ: "e",
-  ẻ: "e",
-  ẽ: "e",
-  ê: "e",
-  ề: "e",
-  ế: "e",
-  ệ: "e",
-  ể: "e",
-  ễ: "e",
-  ì: "i",
-  í: "i",
-  ị: "i",
-  ỉ: "i",
-  ĩ: "i",
-  ò: "o",
-  ó: "o",
-  ọ: "o",
-  ỏ: "o",
-  õ: "o",
-  ô: "o",
-  ồ: "o",
-  ố: "o",
-  ộ: "o",
-  ổ: "o",
-  ỗ: "o",
-  ơ: "o",
-  ờ: "o",
-  ớ: "o",
-  ợ: "o",
-  ở: "o",
-  ỡ: "o",
-  ù: "u",
-  ú: "u",
-  ụ: "u",
-  ủ: "u",
-  ũ: "u",
-  ư: "u",
-  ừ: "u",
-  ứ: "u",
-  ự: "u",
-  ử: "u",
-  ữ: "u",
-  ỳ: "y",
-  ý: "y",
-  ỵ: "y",
-  ỷ: "y",
-  ỹ: "y",
-  đ: "d",
-};
+function removeVietnameseAccents(str: string): string {
+  const vietnameseChars: { [key: string]: string } = {
+    a: "àáảãạâầấẩẫậăằắẳẵặ",
+    e: "èéẻẽẹêềếểễệ",
+    i: "ìíỉĩị",
+    o: "òóỏõọôồốổỗộơờớởỡợ",
+    u: "ùúủũụưừứửữự",
+    y: "ỳýỷỹỵ",
+    d: "đ",
+    A: "ÀÁẢÃẠÂẦẤẨẪẬĂẰẮẲẴẶ",
+    E: "ÈÉẺẼẸÊỀẾỂỄỆ",
+    I: "ÌÍỈĨỊ",
+    O: "ÒÓỎÕỌÔỒỐỔỖỘƠỜỚỞỠỢ",
+    U: "ÙÚỦŨỤƯỪỨỬỮỰ",
+    Y: "ỲÝỶỸỴ",
+    D: "Đ",
+  };
 
-const removeVietnameseChars = (str: string): string => {
-  return str.replace(
-    /[^\u0020-\u007E]/g,
-    (char) => vietnameseCharMap[char] || char
-  );
-};
+  // Duyệt qua từng cặp ký tự có dấu và thay thế
+  Object.keys(vietnameseChars).forEach((nonAccentedChar) => {
+    const accentedChars = vietnameseChars[nonAccentedChar];
+    const regex = new RegExp("[" + accentedChars + "]", "g");
+    str = str.replace(regex, nonAccentedChar);
+  });
+
+  return str;
+}
 
 export const buildSlug = (str: string): string => {
-  const normalizedStr = removeVietnameseChars(str).toLowerCase(); // Chuyển thành chữ thường trước khi chuyển sang slug
+  const normalizedStr = removeVietnameseAccents(str).toLowerCase(); // Chuyển thành chữ thường trước khi chuyển sang slug
   return slugify(normalizedStr, {
     replacement: "-",
     remove: undefined,
