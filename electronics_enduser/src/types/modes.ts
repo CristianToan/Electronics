ed7c08c1605d424bbd6f76e65ba9e44acee2b262
binva12 <quocdat3b@yahoo.com.vn>
2024-11-07 12:22:25 +0700

    dat_admin

--- electronics_enduser/src/types/modes.ts
+++ electronics_enduser/src/types/modes.ts
@@ -17,6 +17,49 @@ export type TProduct = {
   _id: string;
   product_name: string;
 };
+export interface TProducts {
+  _id?: string;
+  product_name: string;
+  price: number;
+  discount: number;
+  category: {
+    _id?: string;
+    category_name: string;
+  };
+  brand: {
+    _id?: string;
+    brand_name: string;
+  };
+  description: string;
+  thumbnail: string;
+  stock: number;
+  slug: string;
+  order: number;
+  isBest: {
+    type: boolean;
+  };
+  isRecentlyAdded: {
+    type: boolean;
+  };
+  isShowHome: {
+    type: boolean;
+  };
+  isDelete: {
+    type: boolean;
+  };
+  specifications: {
+    type: string;
+  };
+}
+
+export interface TProductsCat {
+  _id?: string;
+  category_name: string;
+  description: string;
+  slug: string;
+  order: number;
+  isActive: boolean;
+}
 
 export type TFilterPrice = {
   id: number;
