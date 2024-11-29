adc9fb829fe3e0c889ff23055d5aef097d4f55b3
Tan Hung <maitanhung2@gmail.com>
2024-11-29 07:58:26 +0700

    fix order admin

--- electronics_admin/src/pages/OrderPage.tsx
+++ electronics_admin/src/pages/OrderPage.tsx
@@ -7,6 +7,7 @@ import { axiosClient } from "../lib/axiosClient";
 import { SETTINGS } from "../constants/settings";
 import { Spin } from "antd";
 import { useEffect, useRef, useState } from "react";
+import useAuth from "../hooks/useAuth";
 
 interface TOrder {
   _id: string;
@@ -62,7 +63,8 @@ const OrderPage = () => {
   const [form] = Form.useForm();
   const [params] = useSearchParams();
   const msg = params.get("msg");
-
+  const { user } = useAuth();
+  
   const messageShownRef = useRef(false);
 
   const page_str = params.get("page");
@@ -287,26 +289,28 @@ const OrderPage = () => {
                                       <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path>
                                     </svg>
                                   </button>
-                                  <button
-                                    onClick={() =>
-                                      handleDeleteOrder(order._id!)
-                                    }
-                                    className="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
-                                    aria-label="Delete"
-                                  >
-                                    <svg
-                                      className="w-5 h-5"
-                                      aria-hidden="true"
-                                      fill="currentColor"
-                                      viewBox="0 0 20 20"
+                                  {user?.role == 1 && (
+                                    <button
+                                      onClick={() => {
+                                        handleDeleteOrder(order._id!)
+                                      }}
+                                      className="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+                                      aria-label="Delete"
                                     >
-                                      <path
-                                        fillRule="evenodd"
-                                        d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                                        clipRule="evenodd"
-                                      ></path>
-                                    </svg>
-                                  </button>
+                                      <svg
+                                        className="w-5 h-5"
+                                        aria-hidden="true"
+                                        fill="currentColor"
+                                        viewBox="0 0 20 20"
+                                      >
+                                        <path
+                                          fillRule="evenodd"
+                                          d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                                          clipRule="evenodd"
+                                        ></path>
+                                      </svg>
+                                    </button>
+                                  )}
                                 </div>
                               </td>
                             </tr>
